d406b87f72984d078425e77f25fc33abfee1344c
Derrick Beining <derrick.beining@gmail.com>
2017-10-05 11:30:56 -0400

    removed some commented out code

--- client/redux/reduxUtils.js
+++ client/redux/reduxUtils.js
@@ -6,19 +6,6 @@ import store, {
 } from '../redux';
 const {dispatch} = store;
 
-// import * as bulkActionCreators from '../redux'
-// const {
-//   // updateP1BigStack,
-//   // updateP1DrawnStack,
-//   // updateP1LittleStack,
-//   // updateP1Solitaire1Stack,
-//   // updateP1Solitaire2Stack,
-//   // updateP1Solitaire3Stack,
-//   // updateP1Solitaire4Stack,
-//   updatePlayerStackByKey,
-//   updateFieldStackByKey,
-// } = bulkActionCreators;
-
 export const getFirebaseGameRefFromRedux = () => {
   return store.getState().firebaseRefs.game
 }
@@ -32,9 +19,6 @@ export const storeStackRefInReduxByKey = (stackKey, stackRef) => {
 }
 
 export const updateReduxPlayerStackByKey = (stackKey, newState) => {
-  // const PascalCaseStackKey = stackKey[0].toUpperCase() + stackKey.slice(1);
-  // const actionCreatorKey = `update${PascalCaseStackKey}`
-  // store.dispatch(bulkActionCreators[actionCreatorKey](newState))
   dispatch(updatePlayerStackByKey(stackKey, newState));
 }
 
