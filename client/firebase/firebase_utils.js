624001421e24d0ac6a84c21a048dc5de35922026
Derrick Beining <derrick.beining@gmail.com>
2017-10-05 12:47:38 -0400

    reworking game initializing functions to fit UI flow

--- client/firebase/firebase_utils.js
+++ client/firebase/firebase_utils.js
@@ -17,14 +17,39 @@ const db = firebase.database();
                                  *
                                 * *
                                * * *
-   ------------------------------------------------------------------*/
+------------------------------------------------------------------*/
+
+export const handleAddingPlayerToGame = (playerData, game) => {
+  return goAddPlayerToGame(playerData, game)
+    .then(gameRef => registerHandlerForPlayerUpdates)
+}
+
 export const getSnapshotOfAllPlayersByGameRef = (currentGameRef) => {
   return currentGameRef.child('players').once('value');
+}
+
+
+export const goAddPlayerToGame = (playerData, game) => {
+  const gameRef = typeof game === 'string' ? db.ref('games/${game}') : game;
+  return gameRef.once('value')
+    .then(gameSnapshot => {
+      const playerKey = gameSnapshot.numChildren() + 1;
+      return gameRef.child(`players/${playerKey}`).set(playerData);
+    })
+    .then(() => gameRef)
+    .catch(console.error.bind(console));
 }
 
 export const goCountAllPlayersInGame = () => {
   return getSnapshotOfAllPlayersByGameRef(currentGameRef)
     .then(snapshot => snapshot.numChildren())
+}
+
+const registerHandlerForPlayerUpdates = (gameRef, handler) => {
+  return gameRef.child('players').once('value')
+    .then(snapshotOfAllPlayers => snapshotOfAllPlayers.forEach(player => {
+      player
+    }))
 }
 
 export const setPlayersToGameRef = (players, gameRef) => {
