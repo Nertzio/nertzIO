6d8d66c69cce734bcda412c84a66243091a546d9
Jess Freasier <jfreasier@gmail.com>
2017-10-10 10:57:51 -0500

    signout component added

--- client/firebase/authUtils.js
+++ client/firebase/authUtils.js
@@ -6,7 +6,7 @@ const auth = firebase.auth();
 
   //Event listener for state changes on auth
 export function initAuth () {
-  auth.onAuthStateChanged(user => {
+  Promise.resolve(auth.onAuthStateChanged(user => {
     if (user) {
       console.log("User inside initAuth: ", user.email)
        // Need further clarity on where these values need to be passed initially (i.e. Redux vs Firebase instance). Currently stored on local variable as interim solution for storing values.
@@ -19,7 +19,15 @@ export function initAuth () {
       dispatch(addLocalUserInfo(localUser));
     } else {
       // No user is signed in.
+      let localUser = {
+        displayName: '',
+        email: '',
+      // emailVerified: user.emailVerified,
+        uid: '',
+      }
+      dispatch(addLocalUserInfo(localUser));
       console.log("Not signed in yet / signed out")
     }
-  });
+  }))
+  .then(() => console.log("I listened"));
 }
